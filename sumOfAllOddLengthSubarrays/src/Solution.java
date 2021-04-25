de37d3776c0e74b62e9e35bec7c0db652176d951
Seohui Choe <chelsea.choe@gmail.com>
2021-04-24 21:30:25 -0400

    Add code to calculate the sum of all possible odd-length subarrays

---
+++ sumOfAllOddLengthSubarrays/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
+ * A subarray is a contiguous subsequence of the array.
+ * Return the sum of all odd-length subarrays of arr.
+ */
+public class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int res = 0, n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        }
+        return res;
+    }
+}
