02b63f94b1ff31415487c0f5433a8eb29905b403
Seohui Choe <chelsea.choe@gmail.com>
2021-04-28 22:11:40 -0400

    Add code to return the sum of the matrix diagonals

---
+++ matrixDiagonalSum/src/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * Given a square matrix mat, return the sum of the matrix diagonals.
+ * Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal
+ * that are not part of the primary diagonal.
+ */
+public class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i];
+            if (i != mat.length - 1 - i) {
+                sum += mat[i][mat.length - 1 - i];
+            }
+        }
+        return sum;
+    }
+}
